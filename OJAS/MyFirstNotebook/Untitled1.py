6039c2f7035702adb2fe5f0a242c7bd9eaeaac49
oblivion2611 <170010004@iitdh.ac.in>
2019-03-25 14:32:26 +0530

    First Commit

---
+++ OJAS/MyFirstNotebook/Untitled1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+for i in range(1,int(input())+1):
+    for j in range(1,i+1):
+        print(j, end="   ")
+    print("\n")
+
+
+# In[9]:
+
+
+print("Hello World!");
+
+
+# In[ ]:
+
+
+
+
